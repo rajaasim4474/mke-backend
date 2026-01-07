9a16c58b1de0b3b912af02fc799f4a8818b31db0
rajaasim4 <rajaasim4474@gmail.com>
2026-01-07 19:51:25 +0500

    asd sad sad

--- scraper.js
+++ scraper.js
@@ -721,14 +721,14 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // GET all flavors
 app.get("/api/flavors", async (req, res) => {
